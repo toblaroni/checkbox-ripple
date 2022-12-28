9c9caea97d6b8b63e129e8cae43fa43da286b50a
Toby Hutchinson <tobyhutchinson@gmail.com>
2022-12-28 23:43:52 +0000

    Bugs squashed but when multiple ripples added it ripples from a different origin

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const width = window.innerWidth
 const height = window.innerHeight
-let boxes = 40;
+
+const rippleWidth = 50
+
+let boxes = 30;
 
 let gapWidth = Math.round(width / boxes)
+
+let rippleArr = []
 
 // Class that holds the radius of each ripple
 class Ripple {
@@ -19,12 +24,10 @@ class Ripple {
 window.onload = function(){
    let container = document.getElementById('container')
 
-
    for(let x = 0; x <= width; x += gapWidth){
       for(let y = 0; y <= height + gapWidth; y += gapWidth){
          // Make checkbox
          let checkbox = document.createElement('input')
-
          checkbox.classList.add(x.toString(), y.toString())
          checkbox.type = "checkbox"
 
@@ -40,58 +43,80 @@ window.onload = function(){
 
          // Add to the container
          container.appendChild(checkbox);
-
       }
    }
-
 }
 
 // Get the array of checkboxes
 let checkboxes = document.getElementsByTagName('input')
 
+// make a new ripple when the mouse is clicked
 document.body.addEventListener("click", (e) => {
    if(e.target.type != 'checkbox') return
-   // Create a circle whoes radius expands 
+   // Value that increments untill it's larger than width and height
    let ripple = new Ripple()
 
    // Loop through all of the checkboxes
    for (let checkbox of checkboxes){
       if(checkbox.checked){
-         ripple.originX = checkbox.classList[0]
-         ripple.originY = checkbox.classList[1]
+         if(checkbox.classList.length == 2){
+            ripple.originX = checkbox.classList[0]
+            ripple.originY = checkbox.classList[1]
+         } else {
+            ripple.originX = checkbox.classList[0]
+            ripple.originY = checkbox.classList[0]
+         }
       }
    }
 
-   setInterval(function(){
-      // Check each of the boxes to see whether they should be on
-      // Loop through checkboxes
-      for(let checkbox of checkboxes){
+   // If there's not already a ripple create one
+   rippleArr.push(ripple)
+})
+
+setInterval(function(){
+   console.log(rippleArr)
+   // Check each of the boxes to see whether they should be on
+   // Loop through checkboxes
+   for(let checkbox of checkboxes){
+
+      let x = checkbox.classList[0]
+      let y
+      if(checkbox.classList.length == 1){
+         y = checkbox.classList[0]
+      } else {
+         y = checkbox.classList[1]
+      }
+      
+      // Loop through each ripple to see if the checkbox is within the ripple range
+      for(let r of rippleArr){
          // Calculate the distance from the current box and the checked box
          // Pythagoras
-         let chosenX = ripple.originX
-         let chosenY = ripple.originY
-         let x = checkbox.classList[0]
-         let y = checkbox.classList[1]
+         let chosenX = r.originX
+         let chosenY = r.originY
+         console.log('ChosenX, ChosenY, x, y = ', chosenX, chosenY, x , y)
 
-         if(y == undefined) y = 0;
-         if(chosenY == undefined) chosenY = 0;
+         let X = Math.abs(chosenX - x)
+         let Y = Math.abs(chosenY - y)
 
-         let X = Math.abs(x - chosenX)
-         let Y = Math.abs(y - chosenY)
-
+         // Distance from the clicked checkbox
          let dist = Math.sqrt(Math.pow(X, 2) + Math.pow(Y, 2))
-         
+         console.log("X Y and Dist = ", X, Y, dist)
+
          // If the distance is the same -> check the box. Else uncheck.
-         if(Math.abs(dist - ripple.radius) <= 50){
+         if(Math.abs(dist - r.radius) <= rippleWidth){
             checkbox.checked = true
          } else {
             checkbox.checked = false
-         }
+         }       
       }
-      // Increment the ripples radius
-      ripple.radius += 10
-      if(ripple.radius >= width && ripple.radius >= height){
-         ripple = null
+   }
+
+   // Increment the ripples radius
+   for(let r of rippleArr){
+      if(r.radius >= width * 1.5 && r.radius >= height * 1.5){
+         rippleArr.shift()
+      } else {
+         r.radius += 10
       }
-   }, 10)
-})
+   }
+}, 10)
